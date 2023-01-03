e5bae730e94168daaf849f8020845ffc6ed38f01
KO_Paing_Lay <107101116+Kopaing12@users.noreply.github.com>
2023-01-04 00:59:36 +0630

    Update config.py
    Signed-off-by: KO_Paing_Lay <107101116+Kopaing12@users.noreply.github.com>

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ APP_ID =  "7051195"
 API_HASH = "a36d0269f12722e154f89ff5f0135f04"
 
 # Generate a user session string 
-TG_USER_SESSION = "BQACwrVuVFahR0k9p9h_C5CpL8poDxAqUEJnb8u0bJe18lbch9OV7ftYSKvZckbTuSwjdIvGt28tUbwwy-PDhXJihTfM2y-XRrzz_LuZMQGFg8vLmkWC9D2hIE6tCKZXu_8ASRte5un3AZNWn1P-A_IhA44ZMRpQCKXulxScd3kXmzdZoHQDFFaFF_tMpJUaWJoVBtEBHVV0bHJZfiItR7WXsjt-nVGxF0NbZUo7l2da9w6w9nj4AdeKWJzi6A1u0MAP-VzKhfBnpOLa5Fuh7vKL4wR2VUCHqJxWkmnHsqOL-IQPdOGJ6itmopM6XDzJr-58OhU-pE1_3RY8GUQ8XEr7SEzWqwA"
+TG_USER_SESSION = os.environ.get("TG_USER_SESSION", 'Media_search')
 
 # Database URL from https://cloud.mongodb.com/
 DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://bot1:bot1@cluster0.fqzak9o.mongodb.net/?retryWrites=true&w=majority")
